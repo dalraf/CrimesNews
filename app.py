5fab3b31447047138a8223d328f2e0e49d6eed20
Daniel Fonseca Alves <dalraf@gmail.com>
2022-11-30 15:55:05 -0300

    Adicionado streamlit

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import urllib.parse
 from bs4 import BeautifulSoup
 import requests
 import spacy
+import streamlit as st
 
 nlp = spacy.load("pt_core_news_sm")
 
@@ -45,35 +46,41 @@ def get_text_url(url):
       return ""
 
 
-dados_municipios["Municipio"] = dados_municipios["Municipio"].apply(
-    lambda x: municipio_string_format(x)
-)
-texto_parametros_pesquisa = "delegado,chefe de polícia,chefe de departamento,delegado regional,investigador,escrivão,perito,perícia,médico legista,médico legal,IML,homicídio,feminicídio,roubo,tráfico,lavagem de dinheiro,receptação,furto,arma de fogo,ameaça,ameaçar,ameaçou,foragido"
-lista_parametros_pesquisa = texto_parametros_pesquisa.split(",")
-lista_noticias = []
-for pesquisa in lista_parametros_pesquisa:
-    pesquisa_url = urllib.parse.quote_plus(pesquisa)
-    url = f"https://news.google.com/rss/search?q={pesquisa_url}&hl=pt-BR&gl=BR&ceid=BR%3Apt-419"
-    lista_noticias += feedparser.parse(url)["entries"][:2]
+def executar():
+
+  dados_municipios["Municipio"] = dados_municipios["Municipio"].apply(
+      lambda x: municipio_string_format(x)
+  )
+  texto_parametros_pesquisa = "delegado,chefe de polícia,chefe de departamento,delegado regional,investigador,escrivão,perito,perícia,médico legista,médico legal,IML,homicídio,feminicídio,roubo,tráfico,lavagem de dinheiro,receptação,furto,arma de fogo,ameaça,ameaçar,ameaçou,foragido"
+  lista_parametros_pesquisa = texto_parametros_pesquisa.split(",")
+  lista_noticias = []
+  for pesquisa in lista_parametros_pesquisa:
+      pesquisa_url = urllib.parse.quote_plus(pesquisa)
+      url = f"https://news.google.com/rss/search?q={pesquisa_url}&hl=pt-BR&gl=BR&ceid=BR%3Apt-419"
+      lista_noticias += feedparser.parse(url)["entries"][:2]
 
 
-lista_output = []
-colunas = ["Município", "Regional", "Departamento", "Título", "Links"]
-for news in lista_noticias:
-    titulo = news["title"]
-    link = news["links"][0]["href"]
-    link_text = get_text_url(link)
-    doc_link_text = nlp(link_text)
-    list_ent_gpe = []
-    for ent in doc_link_text.ents:
-        if ent.label_ in ("LOC", "GPE"):
-            list_ent_gpe.append(ent.text)
-    for row in dados_municipios.iterrows():
-        municipio = row[1]["Municipio"]
-        if municipio in list_ent_gpe:
-            regional = row[1]["Regional"]
-            departamento = row[1]["Departamento"]
-            lista_output.append([municipio, regional, departamento, titulo, link])
-dados_crimes = pd.DataFrame(lista_output, columns=colunas)
+  lista_output = []
+  colunas = ["Município", "Regional", "Departamento", "Título", "Links"]
+  for news in lista_noticias:
+      titulo = news["title"]
+      link = news["links"][0]["href"]
+      link_text = get_text_url(link)
+      doc_link_text = nlp(link_text)
+      list_ent_gpe = []
+      for ent in doc_link_text.ents:
+          if ent.label_ in ("LOC", "GPE"):
+              list_ent_gpe.append(ent.text)
+      for row in dados_municipios.iterrows():
+          municipio = row[1]["Municipio"]
+          if municipio in list_ent_gpe:
+              regional = row[1]["Regional"]
+              departamento = row[1]["Departamento"]
+              lista_output.append([municipio, regional, departamento, titulo, link])
+  dados_crimes = pd.DataFrame(lista_output, columns=colunas)
+  return(dados_crimes)
 
-print(dados_crimes)
+
+if st.button('Executar'):
+  df = executar()
+  st.dataframe(df)
